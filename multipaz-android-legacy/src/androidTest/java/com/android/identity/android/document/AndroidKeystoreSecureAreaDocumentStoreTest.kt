koukarine/identity-credential

Multipaz backend server and tightening RPC security
Sample back-end server to support OpenID4VCI protocol.
 - server to mint openid-related attestaions and assertions
 - tweak OpenID4VCI back-end interface to better match the spec
 - simplified secure RPC setup
 - additional checks for Android package names and key mint security level
 - simplified server-side applinks support
 - deleted stale provisioning code

Test: manual testing, additional unit tests

Signed-off-by: Peter Sorotokin <sorotokin@gmail.com>

multipaz-android-legacy/src/androidTest/java/com/android/identity/android/document/AndroidKeystoreSecureAreaDocumentStoreTest.kt
@@ -35,6 +35,7 @@ import kotlinx.io.bytestring.ByteString
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.multipaz.device.AndroidKeyMintSecurityLevel
 import org.multipaz.document.buildDocumentStore
 import org.multipaz.securearea.SecureAreaProvider
 
@@ -87,7 +88,7 @@ class AndroidKeystoreSecureAreaDocumentStoreTest {
         )
         if (!TestUtil.isRunningOnEmulator) {
             Assert.assertEquals(
-                AndroidAttestationExtensionParser.SecurityLevel.TRUSTED_ENVIRONMENT,
+                AndroidKeyMintSecurityLevel.TRUSTED_ENVIRONMENT,
                 parser.keymasterSecurityLevel
             )
         }

multipaz-csa-server/src/main/java/org/multipaz/csa/server/CloudSecureAreaSettings.kt
@@ -10,8 +10,8 @@ class CloudSecureAreaSettings(private val conf: Configuration) {
     val iosReleaseBuild
         get() = getBool("ios_require_release_build", false)
 
-    val iosAppIdentifier
-        get() = getString("ios_require_app_identifier")
+    val iosAppIdentifiers
+        get() = getStringList("ios_require_app_identifiers")
 
     val androidRequireGmsAttestation
         get() = getBool("android_require_gms_attestation", true)
@@ -23,6 +23,12 @@ class CloudSecureAreaSettings(private val conf: Configuration) {
         get() = getStringList("android_require_app_signature_certificate_digests").map {
             ByteString(it.fromBase64Url())
         }
+
+    val androidRequireAppPackageNames: List<String>
+        get() = getStringList("android_require_app_package_names")
+
+    val androidRequireKeyMintSecurityLevel: String?
+        get() = getString("android_require_key_mint_security_level")
 
     val cloudSecureAreaRekeyingIntervalSeconds: Int
         get() = getInt("cloud_secure_area_rekeying_interval_seconds", 300)

multipaz/src/commonTest/kotlin/org/multipaz/device/DeviceAttestationAndroidTest.kt
@@ -3,6 +3,9 @@ package org.multipaz.device
 import org.multipaz.util.fromBase64Url
 import kotlinx.io.bytestring.ByteString
 import kotlinx.io.bytestring.encodeToByteString
+import org.multipaz.crypto.EcPrivateKey
+import org.multipaz.crypto.X509Cert
+import kotlin.collections.setOf
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
 import kotlin.test.Test
@@ -15,12 +18,14 @@ class DeviceAttestationAndroidTest {
             DeviceAttestationValidationData(
                 attestationChallenge = ATTESTATION_CHALLENGE_PIXEL7A.encodeToByteString(),
                 iosReleaseBuild = false,
-                iosAppIdentifier = "",
+                iosAppIdentifiers = setOf(),
                 androidGmsAttestation = true,
                 androidVerifiedBootGreen = true,
-                androidAppSignatureCertificateDigests = listOf(
+                androidRequiredKeyMintSecurityLevel = AndroidKeyMintSecurityLevel.TRUSTED_ENVIRONMENT,
+                androidAppSignatureCertificateDigests = setOf(
                     ByteString("VEpxrWMf2GFLy2_HHTuN7xlW5fy6mKhVAmRADo4aLh0".fromBase64Url())
-                )
+                ),
+                androidAppPackageNames = setOf("org.multipaz_credential.wallet")
             )
         )
     }
@@ -39,12 +44,14 @@ class DeviceAttestationAndroidTest {
             DeviceAttestationValidationData(
                 attestationChallenge = ATTESTATION_CHALLENGE_EMULATOR_PIXEL3A.encodeToByteString(),
                 iosReleaseBuild = false,
-                iosAppIdentifier = "",
+                iosAppIdentifiers = setOf(),
+                androidRequiredKeyMintSecurityLevel = AndroidKeyMintSecurityLevel.SOFTWARE,
                 androidGmsAttestation = false,
                 androidVerifiedBootGreen = false,
-                androidAppSignatureCertificateDigests = listOf(
+                androidAppSignatureCertificateDigests = setOf(
                     ByteString("VEpxrWMf2GFLy2_HHTuN7xlW5fy6mKhVAmRADo4aLh0".fromBase64Url())
-                )
+                ),
+                androidAppPackageNames = setOf("org.multipaz_credential.wallet")
             )
         )
     }
@@ -54,6 +61,38 @@ class DeviceAttestationAndroidTest {
         val deviceAttestation = DeviceAttestation.fromCbor(ATTESTATION_EMULATOR_PIXEL3A)
         val deviceAssertion = DeviceAssertion.fromCbor(ASSERTION_EMULATOR_PIXEL3A)
         deviceAttestation.validateAssertion(deviceAssertion)
+    }
+
+    @Test
+    fun testFoo() {
+        val attestationCertificate = X509Cert.fromPem("""
+            -----BEGIN CERTIFICATE-----
+            MIIBxTCCAUugAwIBAgIJAOQTL9qcQopZMAoGCCqGSM49BAMDMDgxNjA0BgNVBAMT
+            LXVybjp1dWlkOjYwZjhjMTE3LWI2OTItNGRlOC04ZjdmLTYzNmZmODUyYmFhNjAe
+            Fw0yNDA5MjMyMjUxMzFaFw0zNDA5MjMyMjUxMzFaMDgxNjA0BgNVBAMTLXVybjp1
+            dWlkOjYwZjhjMTE3LWI2OTItNGRlOC04ZjdmLTYzNmZmODUyYmFhNjB2MBAGByqG
+            SM49AgEGBSuBBAAiA2IABN4D7fpNMAv4EtxyschbITpZ6iNH90rGapa6YEO/uhKn
+            C6VpPt5RUrJyhbvwAs0edCPthRfIZwfwl5GSEOS0mKGCXzWdRv4GGX/Y0m7EYypo
+            x+tzfnRTmoVX3v6OxQiapKMhMB8wHQYDVR0OBBYEFPqAK5EjiQbxFAeWt//DCaWt
+            C57aMAoGCCqGSM49BAMDA2gAMGUCMEO01fJKCy+iOTpaVp9LfO7jiXcXksn2BA22
+            reiR9ahDRdGNCrH1E3Q2umQAssSQbQIxAIz1FTHbZPcEbA5uE5lCZlRG/DQxlZhk
+            /rZrkPyXFhqEgfMnQ45IJ6f8Utlg+4Wiiw==
+            -----END CERTIFICATE-----
+                """.trimIndent()
+        )
+
+        val attestationPrivateKey = EcPrivateKey.fromPem("""
+                -----BEGIN PRIVATE KEY-----
+                ME4CAQAwEAYHKoZIzj0CAQYFK4EEACIENzA1AgEBBDBn7jeRC9u9de3kOkrt9lLT
+                Pvd1hflNq1FCgs7D+qbbwz1BQa4XXU0SjsV+R1GjnAY=
+                -----END PRIVATE KEY-----
+                """.trimIndent(),
+            attestationCertificate.ecPublicKey
+        )
+
+        val text = attestationPrivateKey.toJwk()
+
+        println(text)
     }
 
     companion object {

multipaz/src/commonTest/kotlin/org/multipaz/device/DeviceAttestationIosTest.kt
@@ -16,10 +16,12 @@ class DeviceAttestationIosTest {
             DeviceAttestationValidationData(
                 attestationChallenge = attestationChallenge.encodeToByteString(),
                 iosReleaseBuild = false,
-                iosAppIdentifier = "74HWMG89B3.com.sorototkin.testapp5",
+                iosAppIdentifiers = setOf("74HWMG89B3.com.sorototkin.testapp5"),
                 androidGmsAttestation = false,
                 androidVerifiedBootGreen = false,
-                androidAppSignatureCertificateDigests = listOf()
+                androidRequiredKeyMintSecurityLevel = AndroidKeyMintSecurityLevel.SOFTWARE,
+                androidAppSignatureCertificateDigests = setOf(),
+                androidAppPackageNames = setOf()
             )
         )
     }

multipaz/src/jvmTest/kotlin/org/multipaz/provisioning/openid4vci/OpenIDBackendUtilTest.kt
@@ -1,0 +1,140 @@
+package org.multipaz.provisioning.openid4vci
+
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import org.junit.Assert
+import org.multipaz.crypto.Crypto
+import org.multipaz.crypto.EcCurve
+import org.multipaz.crypto.EcPublicKey
+import org.multipaz.crypto.SigningKey
+import org.multipaz.jwt.JwtCheck
+import org.multipaz.jwt.validateJwt
+import org.multipaz.rpc.backend.BackendEnvironment
+import org.multipaz.rpc.backend.Configuration
+import org.multipaz.securearea.CreateKeySettings
+import org.multipaz.securearea.software.SoftwareSecureArea
+import org.multipaz.storage.ephemeral.EphemeralStorage
+import org.multipaz.storage.Storage
+import kotlin.reflect.KClass
+import kotlin.reflect.cast
+import kotlin.test.Test
+
+class OpenIDBackendUtilTest {
+    @Test
+    fun testClientAssertion() = runTest {
+        val signingKey = Crypto.createEcPrivateKey(EcCurve.P256)
+        val env = TestBackendEnvironment("client", signingKey.publicKey)
+        withContext(env) {
+            val assertionJwt = OpenIDBackendUtil.createJwtClientAssertion(
+                signingKey = SigningKey.NamedExplicit("client", signingKey),
+                clientId = CLIENT_ID,
+                tokenUrl = "http://example.com",
+            )
+            validateJwt(
+                jwt = assertionJwt,
+                jwtName = "client_assertion",
+                publicKey = null,
+                checks = mapOf(
+                    JwtCheck.TRUST to "fake_trust",  // where to find CA
+                    JwtCheck.JTI to CLIENT_ID,
+                    JwtCheck.SUB to CLIENT_ID,
+                    JwtCheck.ISS to CLIENT_ID,
+                    JwtCheck.AUD to "http://example.com"
+                )
+            )
+        }
+    }
+
+    @Test
+    fun testWalletAttestation() = runTest {
+        val signingKey = Crypto.createEcPrivateKey(EcCurve.P256)
+        val attestedKey = Crypto.createEcPrivateKey(EcCurve.P256).publicKey
+        val env = TestBackendEnvironment("wallet", signingKey.publicKey)
+        withContext(env) {
+            val attestationJwt = OpenIDBackendUtil.createWalletAttestation(
+                signingKey = SigningKey.NamedExplicit("wallet", signingKey),
+                clientId = CLIENT_ID,
+                attestationIssuer = "wallet",
+                attestedKey = attestedKey,
+                nonce = NONCE,
+                walletName = "test",
+                walletLink = "http://example.com"
+            )
+            val attestationBody = validateJwt(
+                jwt = attestationJwt,
+                jwtName = "Client Attestation",
+                publicKey = null,
+                checks = mapOf(
+                    JwtCheck.TRUST to "fake_trust",
+                    JwtCheck.TYP to "oauth-client-attestation+jwt",
+                    JwtCheck.SUB to CLIENT_ID
+                )
+            )
+            val key = EcPublicKey.fromJwk(attestationBody["cnf"]!!.jsonObject["jwk"]!!.jsonObject)
+            Assert.assertEquals(attestedKey, key)
+        }
+    }
+
+    @Test
+    fun testKeyAttestation() = runTest {
+        val signingKey = Crypto.createEcPrivateKey(EcCurve.P256)
+        val env = TestBackendEnvironment("key", signingKey.publicKey)
+        withContext(env) {
+            val ephemeralStorage = EphemeralStorage()
+            val softwareSecureArea = SoftwareSecureArea.create(ephemeralStorage)
+            val attestedKeyInfo = softwareSecureArea.createKey(
+                alias = null,
+                createKeySettings = CreateKeySettings()
+            )
+            val attestationJwt = OpenIDBackendUtil.createJwtKeyAttestation(
+                signingKey = SigningKey.NamedExplicit("key", signingKey),
+                attestationIssuer = "key",
+                keysToAttest = listOf(attestedKeyInfo.attestation),
+                challenge = NONCE
+            )
+            val body = validateJwt(
+                jwt = attestationJwt,
+                jwtName = "Key attestation",
+                publicKey = null,
+                checks = mapOf(
+                    JwtCheck.TYP to "keyattestation+jwt",  // TODO: it is now key-attestation+jwt
+                    JwtCheck.TRUST to "fake_trust"
+                )
+            )
+            val key = EcPublicKey.fromJwk(body["attested_keys"]!!.jsonArray[0].jsonObject)
+            Assert.assertEquals(attestedKeyInfo.publicKey, key)
+        }
+    }
+
+    inner class TestBackendEnvironment(
+        val trustedKeyId: String,
+        val trustedKey: EcPublicKey
+    ): BackendEnvironment, Configuration {
+        val storage = EphemeralStorage()
+
+        override fun <T : Any> getInterface(clazz: KClass<T>): T {
+            return clazz.cast(when (clazz) {
+                Configuration::class -> this
+                Storage::class -> storage
+                else -> throw IllegalArgumentException("no such class available: ${clazz.simpleName}")
+            })
+        }
+
+        override fun getValue(key: String): String? {
+            if (key == "fake_trust") {
+                return buildJsonObject {
+                    put(trustedKeyId, trustedKey.toJwk())
+                }.toString()
+            }
+            return null
+        }
+    }
+
+    companion object {
+        const val CLIENT_ID = "testClientId"
+        const val NONCE = "myNonce"
+    }
+}

openwallet-foundation/multipaz

multipaz-android-legacy/src/androidTest/java/com/android/identity/android/document/AndroidKeystoreSecureAreaDocumentStoreTest.kt
@@ -35,8 +35,8 @@ import kotlinx.io.bytestring.ByteString
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.multipaz.device.AndroidKeystoreSecurityLevel
 import org.multipaz.document.buildDocumentStore
-import org.multipaz.securearea.SecureAreaProvider
 
 // See DocumentStoreTest in non-Android tests for main tests for DocumentStore. These
 // tests are just for the Android-specific bits including attestation.
@@ -87,7 +87,7 @@ class AndroidKeystoreSecureAreaDocumentStoreTest {
         )
         if (!TestUtil.isRunningOnEmulator) {
             Assert.assertEquals(
-                AndroidAttestationExtensionParser.SecurityLevel.TRUSTED_ENVIRONMENT,
+                AndroidKeystoreSecurityLevel.TRUSTED_ENVIRONMENT,
                 parser.keymasterSecurityLevel
             )
         }

multipaz-csa-server/src/main/java/org/multipaz/csa/server/CloudSecureAreaSettings.kt
@@ -10,8 +10,8 @@ class CloudSecureAreaSettings(private val conf: Configuration) {
     val iosReleaseBuild
         get() = getBool("ios_require_release_build", false)
 
-    val iosAppIdentifier
-        get() = getString("ios_require_app_identifier")
+    val iosAppIdentifiers
+        get() = getStringList("ios_require_app_identifiers")
 
     val androidRequireGmsAttestation
         get() = getBool("android_require_gms_attestation", true)
@@ -23,6 +23,12 @@ class CloudSecureAreaSettings(private val conf: Configuration) {
         get() = getStringList("android_require_app_signature_certificate_digests").map {
             ByteString(it.fromBase64Url())
         }
+
+    val androidRequireAppPackageNames: List<String>
+        get() = getStringList("android_require_app_package_names")
+
+    val androidRequireKeystoreSecurityLevel: String?
+        get() = getString("android_require_keystore_security_level")
 
     val cloudSecureAreaRekeyingIntervalSeconds: Int
         get() = getInt("cloud_secure_area_rekeying_interval_seconds", 300)

multipaz/src/commonTest/kotlin/org/multipaz/device/DeviceAttestationAndroidTest.kt
@@ -3,6 +3,9 @@ package org.multipaz.device
 import org.multipaz.util.fromBase64Url
 import kotlinx.io.bytestring.ByteString
 import kotlinx.io.bytestring.encodeToByteString
+import org.multipaz.crypto.EcPrivateKey
+import org.multipaz.crypto.X509Cert
+import kotlin.collections.setOf
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
 import kotlin.test.Test
@@ -15,12 +18,14 @@ class DeviceAttestationAndroidTest {
             DeviceAttestationValidationData(
                 attestationChallenge = ATTESTATION_CHALLENGE_PIXEL7A.encodeToByteString(),
                 iosReleaseBuild = false,
-                iosAppIdentifier = "",
+                iosAppIdentifiers = setOf(),
                 androidGmsAttestation = true,
                 androidVerifiedBootGreen = true,
-                androidAppSignatureCertificateDigests = listOf(
+                androidRequiredKeyMintSecurityLevel = AndroidKeystoreSecurityLevel.TRUSTED_ENVIRONMENT,
+                androidAppSignatureCertificateDigests = setOf(
                     ByteString("VEpxrWMf2GFLy2_HHTuN7xlW5fy6mKhVAmRADo4aLh0".fromBase64Url())
-                )
+                ),
+                androidAppPackageNames = setOf("org.multipaz_credential.wallet")
             )
         )
     }
@@ -39,12 +44,14 @@ class DeviceAttestationAndroidTest {
             DeviceAttestationValidationData(
                 attestationChallenge = ATTESTATION_CHALLENGE_EMULATOR_PIXEL3A.encodeToByteString(),
                 iosReleaseBuild = false,
-                iosAppIdentifier = "",
+                iosAppIdentifiers = setOf(),
+                androidRequiredKeyMintSecurityLevel = AndroidKeystoreSecurityLevel.SOFTWARE,
                 androidGmsAttestation = false,
                 androidVerifiedBootGreen = false,
-                androidAppSignatureCertificateDigests = listOf(
+                androidAppSignatureCertificateDigests = setOf(
                     ByteString("VEpxrWMf2GFLy2_HHTuN7xlW5fy6mKhVAmRADo4aLh0".fromBase64Url())
-                )
+                ),
+                androidAppPackageNames = setOf("org.multipaz_credential.wallet")
             )
         )
     }
@@ -54,6 +61,38 @@ class DeviceAttestationAndroidTest {
         val deviceAttestation = DeviceAttestation.fromCbor(ATTESTATION_EMULATOR_PIXEL3A)
         val deviceAssertion = DeviceAssertion.fromCbor(ASSERTION_EMULATOR_PIXEL3A)
         deviceAttestation.validateAssertion(deviceAssertion)
+    }
+
+    @Test
+    fun testFoo() {
+        val attestationCertificate = X509Cert.fromPem("""
+            -----BEGIN CERTIFICATE-----
+            MIIBxTCCAUugAwIBAgIJAOQTL9qcQopZMAoGCCqGSM49BAMDMDgxNjA0BgNVBAMT
+            LXVybjp1dWlkOjYwZjhjMTE3LWI2OTItNGRlOC04ZjdmLTYzNmZmODUyYmFhNjAe
+            Fw0yNDA5MjMyMjUxMzFaFw0zNDA5MjMyMjUxMzFaMDgxNjA0BgNVBAMTLXVybjp1
+            dWlkOjYwZjhjMTE3LWI2OTItNGRlOC04ZjdmLTYzNmZmODUyYmFhNjB2MBAGByqG
+            SM49AgEGBSuBBAAiA2IABN4D7fpNMAv4EtxyschbITpZ6iNH90rGapa6YEO/uhKn
+            C6VpPt5RUrJyhbvwAs0edCPthRfIZwfwl5GSEOS0mKGCXzWdRv4GGX/Y0m7EYypo
+            x+tzfnRTmoVX3v6OxQiapKMhMB8wHQYDVR0OBBYEFPqAK5EjiQbxFAeWt//DCaWt
+            C57aMAoGCCqGSM49BAMDA2gAMGUCMEO01fJKCy+iOTpaVp9LfO7jiXcXksn2BA22
+            reiR9ahDRdGNCrH1E3Q2umQAssSQbQIxAIz1FTHbZPcEbA5uE5lCZlRG/DQxlZhk
+            /rZrkPyXFhqEgfMnQ45IJ6f8Utlg+4Wiiw==
+            -----END CERTIFICATE-----
+                """.trimIndent()
+        )
+
+        val attestationPrivateKey = EcPrivateKey.fromPem("""
+                -----BEGIN PRIVATE KEY-----
+                ME4CAQAwEAYHKoZIzj0CAQYFK4EEACIENzA1AgEBBDBn7jeRC9u9de3kOkrt9lLT
+                Pvd1hflNq1FCgs7D+qbbwz1BQa4XXU0SjsV+R1GjnAY=
+                -----END PRIVATE KEY-----
+                """.trimIndent(),
+            attestationCertificate.ecPublicKey
+        )
+
+        val text = attestationPrivateKey.toJwk()
+
+        println(text)
     }
 
     companion object {

multipaz/src/commonTest/kotlin/org/multipaz/device/DeviceAttestationIosTest.kt
@@ -16,10 +16,12 @@ class DeviceAttestationIosTest {
             DeviceAttestationValidationData(
                 attestationChallenge = attestationChallenge.encodeToByteString(),
                 iosReleaseBuild = false,
-                iosAppIdentifier = "74HWMG89B3.com.sorototkin.testapp5",
+                iosAppIdentifiers = setOf("74HWMG89B3.com.sorototkin.testapp5"),
                 androidGmsAttestation = false,
                 androidVerifiedBootGreen = false,
-                androidAppSignatureCertificateDigests = listOf()
+                androidRequiredKeyMintSecurityLevel = AndroidKeystoreSecurityLevel.SOFTWARE,
+                androidAppSignatureCertificateDigests = setOf(),
+                androidAppPackageNames = setOf()
             )
         )
     }

multipaz/src/jvmTest/kotlin/org/multipaz/provisioning/openid4vci/OpenIDBackendUtilTest.kt
@@ -1,0 +1,140 @@
+package org.multipaz.provisioning.openid4vci
+
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import org.junit.Assert
+import org.multipaz.crypto.Crypto
+import org.multipaz.crypto.EcCurve
+import org.multipaz.crypto.EcPublicKey
+import org.multipaz.crypto.SigningKey
+import org.multipaz.jwt.JwtCheck
+import org.multipaz.jwt.validateJwt
+import org.multipaz.rpc.backend.BackendEnvironment
+import org.multipaz.rpc.backend.Configuration
+import org.multipaz.securearea.CreateKeySettings
+import org.multipaz.securearea.software.SoftwareSecureArea
+import org.multipaz.storage.ephemeral.EphemeralStorage
+import org.multipaz.storage.Storage
+import kotlin.reflect.KClass
+import kotlin.reflect.cast
+import kotlin.test.Test
+
+class OpenIDBackendUtilTest {
+    @Test
+    fun testClientAssertion() = runTest {
+        val signingKey = Crypto.createEcPrivateKey(EcCurve.P256)
+        val env = TestBackendEnvironment("client", signingKey.publicKey)
+        withContext(env) {
+            val assertionJwt = OpenID4VCIBackendUtil.createJwtClientAssertion(
+                signingKey = SigningKey.NamedExplicit("client", signingKey),
+                clientId = CLIENT_ID,
+                tokenUrl = "http://example.com",
+            )
+            validateJwt(
+                jwt = assertionJwt,
+                jwtName = "client_assertion",
+                publicKey = null,
+                checks = mapOf(
+                    JwtCheck.TRUST to "fake_trust",  // where to find CA
+                    JwtCheck.JTI to CLIENT_ID,
+                    JwtCheck.SUB to CLIENT_ID,
+                    JwtCheck.ISS to CLIENT_ID,
+                    JwtCheck.AUD to "http://example.com"
+                )
+            )
+        }
+    }
+
+    @Test
+    fun testWalletAttestation() = runTest {
+        val signingKey = Crypto.createEcPrivateKey(EcCurve.P256)
+        val attestedKey = Crypto.createEcPrivateKey(EcCurve.P256).publicKey
+        val env = TestBackendEnvironment("wallet", signingKey.publicKey)
+        withContext(env) {
+            val attestationJwt = OpenID4VCIBackendUtil.createWalletAttestation(
+                signingKey = SigningKey.NamedExplicit("wallet", signingKey),
+                clientId = CLIENT_ID,
+                attestationIssuer = "wallet",
+                attestedKey = attestedKey,
+                nonce = NONCE,
+                walletName = "test",
+                walletLink = "http://example.com"
+            )
+            val attestationBody = validateJwt(
+                jwt = attestationJwt,
+                jwtName = "Client Attestation",
+                publicKey = null,
+                checks = mapOf(
+                    JwtCheck.TRUST to "fake_trust",
+                    JwtCheck.TYP to "oauth-client-attestation+jwt",
+                    JwtCheck.SUB to CLIENT_ID
+                )
+            )
+            val key = EcPublicKey.fromJwk(attestationBody["cnf"]!!.jsonObject["jwk"]!!.jsonObject)
+            Assert.assertEquals(attestedKey, key)
+        }
+    }
+
+    @Test
+    fun testKeyAttestation() = runTest {
+        val signingKey = Crypto.createEcPrivateKey(EcCurve.P256)
+        val env = TestBackendEnvironment("key", signingKey.publicKey)
+        withContext(env) {
+            val ephemeralStorage = EphemeralStorage()
+            val softwareSecureArea = SoftwareSecureArea.create(ephemeralStorage)
+            val attestedKeyInfo = softwareSecureArea.createKey(
+                alias = null,
+                createKeySettings = CreateKeySettings()
+            )
+            val attestationJwt = OpenID4VCIBackendUtil.createJwtKeyAttestation(
+                signingKey = SigningKey.NamedExplicit("key", signingKey),
+                attestationIssuer = "key",
+                keysToAttest = listOf(attestedKeyInfo.attestation),
+                challenge = NONCE
+            )
+            val body = validateJwt(
+                jwt = attestationJwt,
+                jwtName = "Key attestation",
+                publicKey = null,
+                checks = mapOf(
+                    JwtCheck.TYP to "keyattestation+jwt",  // TODO: it is now key-attestation+jwt
+                    JwtCheck.TRUST to "fake_trust"
+                )
+            )
+            val key = EcPublicKey.fromJwk(body["attested_keys"]!!.jsonArray[0].jsonObject)
+            Assert.assertEquals(attestedKeyInfo.publicKey, key)
+        }
+    }
+
+    inner class TestBackendEnvironment(
+        val trustedKeyId: String,
+        val trustedKey: EcPublicKey
+    ): BackendEnvironment, Configuration {
+        val storage = EphemeralStorage()
+
+        override fun <T : Any> getInterface(clazz: KClass<T>): T {
+            return clazz.cast(when (clazz) {
+                Configuration::class -> this
+                Storage::class -> storage
+                else -> throw IllegalArgumentException("no such class available: ${clazz.simpleName}")
+            })
+        }
+
+        override fun getValue(key: String): String? {
+            if (key == "fake_trust") {
+                return buildJsonObject {
+                    put(trustedKeyId, trustedKey.toJwk())
+                }.toString()
+            }
+            return null
+        }
+    }
+
+    companion object {
+        const val CLIENT_ID = "testClientId"
+        const val NONCE = "myNonce"
+    }
+}